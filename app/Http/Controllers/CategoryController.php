de5debe3cc76c82ab52a34b671373ed60e021357
Billy <hsherlock22@gmail.com>
2021-09-17 14:21:29 +0700

    temp

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Get all Categories.
+     *
+     * @return json
+     */
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json([
+            'error' => false,
+            'data' => $categories
+        ]);
+    }
+
+    /**
+     * Create Category.
+     *
+     * @return json
+     */
+    public function store(CategoryRequest $request)
+    {
+        $category = new Category();
+        $category->name = $request->name;
+        $category->save();
+
+        return response()->json([
+            'error' => false,
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Create Category.
+     *
+     * @return json
+     */
+    public function update(CategoryRequest $request, Category $category)
+    {
+        // $category =  Category::findOrFail($id);
+        $category->name = $request->name;
+        $category->save();
+
+        return response()->json([
+            'error' => false,
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Create Category.
+     *
+     * @return json
+     */
+    public function destroy(Category $category)
+    {
+        // $category =  Category::findOrFail($id);
+        $category->delete();
+
+        return response()->json([
+            'error' => false,
+            'data' => $category
+        ]);
+    }
+
+    /**
+     * Create Category.
+     *
+     * @return json
+     */
+    public function show(Category $category)
+    {
+        return response()->json([
+            'error' => false,
+            'data' => $category
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('hello', function () {
-    return response()->json('hello', 404);
-});
+// Route::get('category', [CategoryController::class, 'index']);
+// Route::post('category', [CategoryController::class, 'store']);
+// Route::put('category/{category}', [CategoryController::class, 'update']);
+// Route::delete('category/{category}', [CategoryController::class, 'destroy']);
+// Route::get('category/{category}', [CategoryController::class, 'show']);
+
+Route::apiResource('category', CategoryController::class);
